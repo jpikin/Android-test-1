070110c9f135679a4ce1be2c072eff00a60c72bd
Pikin <pikin@bk.ru>
2024-04-10 17:20:53 +0500

    < 0 exeption added

--- app/src/main/java/com/example/first/MainActivity.kt
+++ app/src/main/java/com/example/first/MainActivity.kt
@@ -12,17 +12,20 @@ class MainActivity : AppCompatActivity() {
         val binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
         binding.textView!!.text=binding.textView.context.getText((R.string.testText))
-        binding.b1!!.text ="ADD"
-        binding.b2!!.text ="DEL"
+        binding.b1!!.text =binding.textView.context.getText((R.string.b1Text))
+        binding.b2!!.text =binding.textView.context.getText((R.string.b2Text))
         binding.counter!!.text = counter.toString()
 
         binding.b1.setOnClickListener {
             counter++
-            binding.counter!!.text = counter.toString()
+            binding.counter.text = counter.toString()
         }
         binding.b2.setOnClickListener {
-            counter--
-            binding.counter!!.text = counter.toString()
+
+            if(counter>0) counter--
+
+            binding.counter.text = counter.toString()
+
         }
 
 
